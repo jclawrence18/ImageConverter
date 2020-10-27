37e4f47938dd5a68c78238372dfd36ab706746f0
jclawrence18 <30583677+jclawrence18@users.noreply.github.com>
2020-10-26 22:02:34 -0500

    Initial commit

---
+++ ImageConverter/ImageConverter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace ImageConverter
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Instructions: Run image converter from the folder of the images you'd like to convert.");
+
+            var filePath = AppDomain.CurrentDomain.BaseDirectory;
+
+            Console.WriteLine("Running Image Converter...");
+
+            string[] files = Directory.GetFiles(filePath, "*.heic", SearchOption.AllDirectories);
+
+            foreach (var file in files)
+            {
+                var convertedFile = Path.ChangeExtension(file, ".jpeg");
+                System.IO.File.Move(file, convertedFile);
+                Console.WriteLine("Converting {0} to {1}", file, convertedFile);
+            }
+
+            Console.WriteLine("Done!");
+        }
+    }
+}
